c0cf99dec6c7ed08eac298f310f953fae678f4ac
gregwhitaker <gwhitake@gmail.com>
2018-09-21 17:33:31 -0700

    Doing work

--- dubbo-example-provider/src/main/java/dubbo/example/provider/Provider.java
+++ dubbo-example-provider/src/main/java/dubbo/example/provider/Provider.java
@@ -13,9 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package dubbo.example.api;
+package dubbo.example.provider;
 
-public interface DemoService {
+public class Provider {
 
-    String sayHello(String name);
+    public static void main(String... args) {
+
+    }
 }
